afa347610bbf5d164fb92c843bd2c9a4ee337744
naokimidori <naokimidori@163.com>
2024-01-22 22:38:19 +0800

    chore: 路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
-import Login from "@/pages/Login";
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
+import { routesConfig } from './routes';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Navigate to='/login' />} />
+        {routesConfig.map((item) => (
+          <Route key={item.path} path={item.path} element={item.component} />
+        ))}
       </Route>
     </Routes>
   );
