c88b6a9bede046430a30e1d16daad565b379b0dc
vino <naokimidori@163.com>
2024-03-17 21:22:57 +0800

    feat: login page

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,35 +1,45 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from '../Header';
 import Menu from '../Menu';
 import useShowMenu from '@/hooks/useShowMenu';
 import useShowHeader from '@/hooks/useShowHeader';
+import usePathKey from '@/hooks/usePathKey';
 
+const FULL_SCREEN_PATH_KEY = ['login'];
 
 const Layout: React.FC = () => {
-
   const showMenu = useShowMenu();
   const showHeader = useShowHeader();
+  const pathKey = usePathKey();
+
+  if (FULL_SCREEN_PATH_KEY.includes(pathKey)) {
+    return (
+      <div className="layout">
+        <Outlet />
+      </div>
+    );
+  }
 
   return (
     <div className="layout">
       {showHeader && (
-        <div className='layout-header'>
+        <div className="layout-header">
           <Header />
         </div>
       )}
-      <div className='layout-content'>
+      <div className="layout-content">
         {showMenu && (
-          <div className='layout-nav'>
+          <div className="layout-nav">
             <Menu />
           </div>
         )}
-        <div className='layout-outlet'>
+        <div className="layout-outlet">
           <Outlet />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,120 @@
-import React, { useState, useEffect } from 'react'
+import {
+  LockOutlined,
+  MobileOutlined,
+} from '@ant-design/icons';
+import {
+  LoginFormPage,
+  ProConfigProvider,
+  ProFormCaptcha,
+  ProFormCheckbox,
+  ProFormText,
+} from '@ant-design/pro-components';
+import { message, theme } from 'antd';
+import styles from './index.module.scss';
 
-function Login() {
+const Page = () => {
+  const { token } = theme.useToken();
+  return (
+    <div
+      className={styles.loginPage}
+      style={{
+        backgroundColor: 'white',
+        height: '100vh',
+      }}
+    >
+      <LoginFormPage
+        backgroundVideoUrl="https://gw.alipayobjects.com/v/huamei_gcee1x/afts/video/jXRBRK_VAwoAAAAAAAAAAAAAK4eUAQBr"
+        title="考试评测系统"
+        containerStyle={{
+          backgroundColor: 'rgba(0, 0, 0,0.65)',
+          backdropFilter: 'blur(4px)',
+        }}
+      >
+        <>
+          <ProFormText
+            fieldProps={{
+              size: 'large',
+              prefix: (
+                <MobileOutlined
+                  style={{
+                    color: token.colorText,
+                  }}
+                  className={'prefixIcon'}
+                />
+              ),
+            }}
+            name="mobile"
+            placeholder={'手机号'}
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号！',
+              },
+              {
+                pattern: /^1\d{10}$/,
+                message: '手机号格式错误！',
+              },
+            ]}
+          />
+          <ProFormCaptcha
+            fieldProps={{
+              size: 'large',
+              prefix: (
+                <LockOutlined
+                  style={{
+                    color: token.colorText,
+                  }}
+                  className={'prefixIcon'}
+                />
+              ),
+            }}
+            captchaProps={{
+              size: 'large',
+            }}
+            placeholder={'请输入验证码'}
+            captchaTextRender={(timing, count) => {
+              if (timing) {
+                return `${count} ${'获取验证码'}`;
+              }
+              return '获取验证码';
+            }}
+            name="captcha"
+            rules={[
+              {
+                required: true,
+                message: '请输入验证码！',
+              },
+            ]}
+            onGetCaptcha={async () => {
+              message.success('获取验证码成功！验证码为：1234');
+            }}
+          />
+        </>
+        <div
+          style={{
+            marginBlockEnd: 24,
+          }}
+        >
+          <ProFormCheckbox noStyle name="autoLogin">
+            自动登录
+          </ProFormCheckbox>
+          <a
+            style={{
+              float: 'right',
+            }}
+          >
+            忘记密码
+          </a>
+        </div>
+      </LoginFormPage>
+    </div>
+  );
+};
 
-  useEffect(() => {
-  }, []);
-
-  return <h1>login</h1>
-}
-
-export default Login;
+export default () => {
+  return (
+    <ProConfigProvider dark>
+      <Page />
+    </ProConfigProvider>
+  );
+};
